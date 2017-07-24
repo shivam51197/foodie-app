4ec981f1d0ce5d91447fe6b5b78519ad895952d2
Shivam <krshivam58@gmail.com>
2017-07-24 16:19:41 +0530

    new started project

---
+++ Class 1/main.js
@@ -0,0 +1,14 @@
+/* Runs when HTML file is loaded
+    $(document).ready(function() {
+    $('#user-email').on('keypress',function() {
+       console.log($('#user-email').val())
+   });
+});*/
+
+/*FOR welcome message
+$('#user-email').on('input',function() {
+$('#user-email').on('keypress',function() {
+        var email = $('#user-email').val()
+        var message = 'Welcome Back, ' + email;
+        $('.welcome-message').text(message);
+    });*/
